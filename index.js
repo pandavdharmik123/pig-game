b357eac2b4220ac516b846640a6b62f1ba1efae5
dharmikpandav1311 <dhamopandav1311@gmail.com>
2021-02-08 16:15:20 +0530

    Add the express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+const PORT = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname) 
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(PORT, ()=> {
+    console.log('Server starts on ', PORT)
+})
